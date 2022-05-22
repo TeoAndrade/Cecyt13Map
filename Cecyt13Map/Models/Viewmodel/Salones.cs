da9f2a0fbbddbc5315b7506b48364db8ab3ac997
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-05-21 21:33:44 -0500

    Barra de busqueda y archivos

---
+++ Cecyt13Map/Models/Viewmodel/Salones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cecyt13Map.Models.Viewmodel
+{
+    public class Salones
+    {
+        public int Cve_Salon { get; set; }
+        public string Nom_salon { get; set; }
+        public string Piso { get; set; }
+        public UbicacionViewModel IdUbi { get; set; }
+    }
+}
