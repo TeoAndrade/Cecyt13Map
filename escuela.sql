cf1fbd4020e4dd3a90fcb904fe9b50e566e6db08
TeoAndrade <105954698+TeoAndrade@users.noreply.github.com>
2022-05-25 18:41:40 -0500

    Update escuela.sql
    * Update escuela.sql
    
    * Update escuela.sql

--- escuela.sql
+++ escuela.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE Cecyt13Map2
 
 USE Cecyt13Map2
-
+--proseso de registro de usuario
 CREATE TABLE Usuario(
     Cve_Usuario int PRIMARY KEY IDENTITY (1,1),
     Nom_Usuario VARCHAR(50),
     Correo VARCHAR(max),
     Contraseña VARCHAR(max)
 )
-
+--proseso de creacion de la tabla de ubicacion
 CREATE TABLE Ubicacion(
     Cve_Ubicacion int PRIMARY KEY IDENTITY (100,100),
     Nom_Ubicacion VARCHAR(50)
 )
-
+--proceso de creacion de la tabla de escuela
 CREATE TABLE Escuela(
     Cve_Escuela int PRIMARY KEY IDENTITY(1,1),
     Nom_Escuela VARCHAR(50),
@@ -21,7 +21,7 @@ CREATE TABLE Escuela(
     Id_Ubicacion int,
     FOREIGN KEY (Id_Ubicacion) REFERENCES Ubicacion(Cve_Ubicacion)
 )
-
+--creacion de listado de salones
 CREATE PROCEDURE sp_Lista_Salones
     @EdificioID int
 AS
@@ -30,7 +30,7 @@ BEGIN
 	where @EdificioID=Cve_Ubicacion
 END
 GO
-
+--creacion del procedimiento registro de usuario
 CREATE PROCEDURE SP_Login(
 @correo VARCHAR(max),
     @contra VARCHAR(max)
@@ -41,7 +41,7 @@ BEGIN
     ELSE
         SELECT '0'
 end 
-
+--procedimientro de registro
 CREATE PROCEDURE SP_Registrar(
     @correo VARCHAR(max),
     @contra VARCHAR(max),
@@ -49,6 +49,7 @@ CREATE PROCEDURE SP_Registrar(
     @Registrado BIT OUTPUT,--! Datos de salida
     @mensaje VARCHAR(100) OUTPUT
 )as
+-- en caso de que el usuario se haya registrado anteriormente
 BEGIN
     if(not exists(select*from Usuario where Correo=@correo))
     BEGIN
@@ -63,7 +64,7 @@ BEGIN
     END
 end
 go
-
+--insertar nombre de edificio
 INSERT into Ubicacion VALUES
 ('Edificio Norte'),
 ('Edificio Sur'),
@@ -71,7 +72,7 @@ INSERT into Ubicacion VALUES
 ('Gestion Escolar'),
 ('Edificio Norponiente'),
 ('Edificio Poniente');
-
+--insertar nombre de salones
 insert into Escuela values
 ('Sala Siglo XXI','PB',100),
 ('Dibujo','PB',100),
@@ -151,7 +152,7 @@ insert into Escuela values
 ('6IV20','PB',500),
 ('6IV21','1',600),
 ('6IV22','1',600);
-
+--proceso de  bbusqueda de salones
 create PROCEDURE BuscarEscuela 
 	@consulta varchar(50)
 AS
@@ -160,7 +161,7 @@ BEGIN
 	inner join Ubicacion on Escuela.Id_Ubicacion=Ubicacion.Cve_Ubicacion
 	where CONCAT(Nom_Escuela,'-',Piso,'-',Id_Ubicacion) like '%'+@consulta+'%'
 END
-
+--proceso de editar datos de administrador
 Create PROCEDURE EditarAdmin
 	-- Add the parameters for the stored procedure here
 	@Cve_Admin int,
@@ -172,6 +173,7 @@ Create PROCEDURE EditarAdmin
 	@Resultado int output
 AS
 BEGIN
+--registro de un administrador
 	set @Resultado=0
 	if not exists(select *from Administradores where CorreoAdmi=@Correo and CveAdmin=@Cve_Admin)
 	begin
@@ -184,9 +186,10 @@ BEGIN
 		set @Resultado=1
 	end
 	else
+	-- en caso de que el correo que ingrese el admin , haya sido anteriormente usado
 		set @Mensaje='El correo ya existe'
 END
-
+--proceso de registro admin
 create PROCEDURE [dbo].[RegistrarAdmin]
 	-- Add the parameters for the stored procedure here
 	@Nombre varchar(50),
@@ -197,6 +200,7 @@ create PROCEDURE [dbo].[RegistrarAdmin]
 	@Resultado int output
 AS
 BEGIN
+--en caso de que el aministrador no este registrado
 	set @Resultado=0
 	if not exists(select *from Administradores where CorreoAdmi=@Correo)
 	begin
@@ -207,6 +211,7 @@ BEGIN
 	else
 		set @Mensaje='El correo ya existe'
 END
+--proceso de ,crear edificio
 Create PROCEDURE [dbo].[CrearEdificio]
 	-- Add the parameters for the stored procedure here
 	@Nombre varchar(50),
@@ -214,6 +219,7 @@ Create PROCEDURE [dbo].[CrearEdificio]
 	@Resultado int output
 AS
 BEGIN
+--en caso de que no exista la ubicacion buscada
 	set @Resultado=0
 	if not exists(select *from Ubicacion where Nom_Ubicacion=@Nombre)
 	begin
@@ -222,9 +228,10 @@ BEGIN
 		set @Resultado=1
 	end
 	else
+	--se mostrara el siguiente mensaje
 		set @Mensaje='El edificio ya existe'
 END
-
+--proceso de, editar datos de algun edificio
 Create PROCEDURE [dbo].[EditarEdificio] 
 	-- Add the parameters for the stored procedure here
 	@IdEdificio int,
@@ -233,6 +240,7 @@ Create PROCEDURE [dbo].[EditarEdificio]
 	@Resultado int output
 AS
 BEGIN
+--proceso en caso de que el edificio que quieran agregar ya exista
 	set @Resultado=0
 	if not exists(select *from Ubicacion where Nom_Ubicacion=@Nombre and Cve_Ubicacion!=@IdEdificio)
 	begin
@@ -244,7 +252,7 @@ BEGIN
 	else
 		set @Mensaje='El edificio ya existe'
 END
-
+-- proceso de eliminar edifio
 Create PROCEDURE [dbo].[EliminarEdificio] 
 	-- Add the parameters for the stored procedure here
 	@IdEdificio int,
@@ -252,6 +260,7 @@ Create PROCEDURE [dbo].[EliminarEdificio]
 	@Resultado int output
 AS
 BEGIN
+--proceso de, relaciones edificio-escuela
 	set @Resultado=0
 	if not exists(select *from Ubicacion inner join Escuela on Escuela.Id_Ubicacion=Ubicacion.Cve_Ubicacion
 	where Escuela.Cve_Escuela=@IdEdificio)
@@ -263,7 +272,7 @@ BEGIN
 	else
 		set @Mensaje='El edificio esta relacionado a un salon'
 END
-
+--proceso de creacion de tabla de administradores
 create table Administradores(
 Cve_Admin int primary key identity(1,1),
 Nombre varchar(50),
@@ -273,7 +282,7 @@ Contrasena varchar(max)
 
 )
 
-
+--proceso de crear nuevo salon
 CREATE PROCEDURE [dbo].[CrearSalon] 
 	-- Add the parameters for the stored procedure here
 	@Nombre varchar(100),
@@ -283,6 +292,7 @@ CREATE PROCEDURE [dbo].[CrearSalon]
 	@Resultado int output
 AS
 BEGIN
+-- proceso en caso de que el salon que se agregue ya exista
 	set @Resultado=0
 	if not exists(select *from Escuela where @Nombre=Nom_Escuela)
 	begin
@@ -293,7 +303,7 @@ BEGIN
 	else
 		set @Mensaje='El salon ya existe'
 END
-
+--proceso de edicion de algun salon
 CREATE PROCEDURE [dbo].[EditarSalon] 
 	-- Add the parameters for the stored procedure here
 	@CveEscuela int,
@@ -304,6 +314,7 @@ CREATE PROCEDURE [dbo].[EditarSalon]
 	@Resultado int output
 AS
 BEGIN
+--proceso en caso de que el salon que se agregue ya exista
 	set @Resultado=0
 	if not exists(select *from Escuela where @Nombre=Nom_Escuela and @CveEscuela!=Cve_Escuela)
 	begin
@@ -317,7 +328,7 @@ BEGIN
 	else
 		set @Mensaje='El salon ya existe'
 END
-
+-- tabla de administradores
 create table Administradores(
 CveAdmin int primary key identity (20,20),
 NombreAdmin varchar(50),
